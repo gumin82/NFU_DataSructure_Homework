45986bd57e7093d65f17065df61cf364f89545b2
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2026-05-03 19:41:18 +0800

    Implement adjacency list for unweighted and weighted graphs

---
+++ homework2(2)/src/AdjacencyList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int V,E;
+    cin>>V>>E;
+
+    vector<vector<int>> unweighted(V);
+    vector<vector<pair<int,int>>> weighted(V);
+
+    for(int i=0;i<E;i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+
+        unweighted[u].push_back(v);
+        unweighted[v].push_back(u);
+
+        weighted[u].push_back({v,w});
+        weighted[v].push_back({u,w});
+    }
+
+    cout<<"Unweighted List:\n";
+    for(int i=0;i<V;i++){
+        cout<<i<<": ";
+        for(int x: unweighted[i])
+            cout<<x<<" ";
+        cout<<endl;
+    }
+
+    cout<<"\nWeighted List:\n";
+    for(int i=0;i<V;i++){
+        cout<<i<<": ";
+        for(auto x: weighted[i])
+            cout<<"("<<x.first<<","<<x.second<<") ";
+        cout<<endl;
+    }
+}
