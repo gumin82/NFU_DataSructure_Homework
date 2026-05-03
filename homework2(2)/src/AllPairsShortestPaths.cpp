cb9c54ebaa402716c789af626bb8b0caacfaf988
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2026-05-03 20:19:40 +0800

    Implement Floyd-Warshall algorithm for shortest paths

---
+++ homework2(2)/src/AllPairsShortestPaths.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int V;
+    cin >> V;
+
+    int dist[100][100];
+
+    for(int i=0;i<V;i++)
+        for(int j=0;j<V;j++)
+            cin >> dist[i][j];
+
+    for(int k=0;k<V;k++)
+        for(int i=0;i<V;i++)
+            for(int j=0;j<V;j++)
+                if(dist[i][k]+dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k]+dist[k][j];
+
+    cout<<"Result:\n";
+    for(int i=0;i<V;i++){
+        for(int j=0;j<V;j++)
+            cout<<dist[i][j]<<" ";
+        cout<<endl;
+    }
+}
