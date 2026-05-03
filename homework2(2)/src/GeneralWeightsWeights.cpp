c2c72e4d1071846ede1ddc9aaf3a3edb3020cbd2
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2026-05-03 20:18:31 +0800

    Add GeneralWeightsWeights.cpp for shortest path calculation
    Implement Dijkstra's algorithm to find shortest paths.

---
+++ homework2(2)/src/GeneralWeightsWeights.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Edge{
+    int u,v,w;
+};
+
+int main(){
+    int V,E;
+    cin>>V>>E;
+
+    vector<Edge> edges(E);
+    for(int i=0;i<E;i++)
+        cin>>edges[i].u>>edges[i].v>>edges[i].w;
+
+    int start;
+    cin>>start;
+
+    vector<int> dist(V,1e9);
+    dist[start]=0;
+
+    for(int i=0;i<V-1;i++){
+        for(auto e: edges){
+            if(dist[e.u]!=1e9 && dist[e.v] > dist[e.u]+e.w)
+                dist[e.v]=dist[e.u]+e.w;
+        }
+    }
+
+    for(int i=0;i<V;i++)
+        cout<<"dist["<<i<<"]="<<dist[i]<<endl;
+}
