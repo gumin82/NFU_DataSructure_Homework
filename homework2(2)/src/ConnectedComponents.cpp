313f03c24169bcfdbfb82af07f1e83f58c22414d
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2026-05-03 19:58:02 +0800

    Add ConnectedComponents.cpp for DFS implementation

---
+++ homework2(2)/src/ConnectedComponents.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFS(int v, vector<vector<int>>& adj, vector<bool>& visited){
+    visited[v]=true;
+    cout<<v<<" ";
+
+    for(int u: adj[v])
+        if(!visited[u])
+            DFS(u,adj,visited);
+}
+
+int main(){
+    int V,E;
+    cin>>V>>E;
+
+    vector<vector<int>> adj(V);
+
+    for(int i=0;i<E;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> visited(V,false);
+    int count=0;
+
+    for(int i=0;i<V;i++){
+        if(!visited[i]){
+            cout<<"Component "<<++count<<": ";
+            DFS(i,adj,visited);
+            cout<<endl;
+        }
+    }
+}
