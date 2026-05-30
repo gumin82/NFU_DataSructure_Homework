bf40a01b5a06c2d3c99501a093e35e5c2629d1b0
kang2486 <41343122@nfu.edu.tw>
2026-05-30 19:04:19 +0800

    Update insertion_worst.cpp

--- homework3(2)/src/generator/insertion_worst.cpp
+++ homework3(2)/src/generator/insertion_worst.cpp
@@ -1,1 +1,38 @@
+//41343133
+//41343122
+#include <iostream>
+using namespace std;
 
+// Insertion Sort Worst Case
+void generateInsertionWorstCase(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = n - i;
+    }
+}
+
+// 測試用
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int* arr = new int[n];
+
+    generateInsertionWorstCase(arr, n);
+
+    cout << "Insertion Sort Worst Case:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
