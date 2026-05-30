a072c06a8dbe74f3d5a073891ba136d50e903c3d
kang2486 <41343122@nfu.edu.tw>
2026-05-30 19:04:51 +0800

    Update random_permutation.cpp

--- homework3(2)/src/generator/random_permutation.cpp
+++ homework3(2)/src/generator/random_permutation.cpp
@@ -1,1 +1,52 @@
+//41343133
+//41343122
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+// Random Permutation Generator
+void generateRandomPermutation(int arr[], int n)
+{
+    // 初始化
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    // Fisher-Yates Shuffle
+    for(int i = n - 1; i >= 1; i--)
+    {
+        int j = rand() % (i + 1);
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int n;
+
+    cin >> n;
+
+    int* arr = new int[n];
+
+    generateRandomPermutation(arr, n);
+
+    cout << "Random Permutation:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
