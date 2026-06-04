59f3585649ba06933cf9a6ce08781ac9ca989e09
kang2486 <41343122@nfu.edu.tw>
2026-06-04 23:25:26 +0800

    Update merge_sort.cpp

--- homework3(2)/src/sorting/merge_sort.cpp
+++ homework3(2)/src/sorting/merge_sort.cpp
@@ -2,82 +2,63 @@
 //41343122
 #include <iostream>
 using namespace std;
-
-void merge(int arr[],
-           int temp[],
-           int leftStart,
-           int rightStart,
-           int rightEnd)
+void mergeArray(
+    int arr[],
+    int temp[],
+    int left,
+    int mid,
+    int right)
 {
-    int leftEnd=rightStart-1;
-    int tempPos=leftStart;
+    int i = left;
+    int j = mid + 1;
+    int k = left;
 
-    while(leftStart<=leftEnd &&
-          rightStart<=rightEnd)
+    while(i <= mid && j <= right)
     {
-        if(arr[leftStart]<=arr[rightStart])
-            temp[tempPos++]=arr[leftStart++];
+        if(arr[i] <= arr[j])
+            temp[k++] = arr[i++];
         else
-            temp[tempPos++]=arr[rightStart++];
+            temp[k++] = arr[j++];
     }
 
-    while(leftStart<=leftEnd)
-        temp[tempPos++]=arr[leftStart++];
+    while(i <= mid)
+        temp[k++] = arr[i++];
 
-    while(rightStart<=rightEnd)
-        temp[tempPos++]=arr[rightStart++];
+    while(j <= right)
+        temp[k++] = arr[j++];
 
-    for(int i=0;i<=rightEnd;i++)
-        arr[i]=temp[i];
+    for(int p = left; p <= right; p++)
+        arr[p] = temp[p];
 }
 
-void iterativeMergeSort(int arr[],int n)
+void iterativeMergeSort(int arr[], int n)
 {
-    int* temp=new int[n];
+    int* temp = new int[n];
 
-    for(int size=1;size<n;size*=2)
+    for(int size = 1; size < n; size *= 2)
     {
-        for(int left=0;
-            left<n-1;
-            left+=2*size)
+        for(int left = 0;
+            left < n - 1;
+            left += 2 * size)
         {
-            int mid=left+size-1;
+            int mid = left + size - 1;
 
-            int right=
-            left+2*size-1;
-
-            if(mid>=n-1)
+            if(mid >= n - 1)
                 continue;
 
-            if(right>=n)
-                right=n-1;
+            int right = left + 2 * size - 1;
 
-            merge(arr,temp,left,
-                  mid+1,right);
+            if(right >= n)
+                right = n - 1;
+
+            mergeArray(
+                arr,
+                temp,
+                left,
+                mid,
+                right);
         }
     }
 
     delete[] temp;
-}
-
-int main()
-{
-    int n;
-    cin>>n;
-
-    int* arr=new int[n];
-
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-
-    iterativeMergeSort(arr,n);
-
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
-
-    cout<<endl;
-
-    delete[] arr;
-
-    return 0;
 }
