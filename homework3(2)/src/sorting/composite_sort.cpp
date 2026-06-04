72ef7d5c65443bcec04bab75d4fdae6c31e1b1f9
kang2486 <41343122@nfu.edu.tw>
2026-06-05 00:16:55 +0800

    Create composite_sort.cpp

---
+++ homework3(2)/src/sorting/composite_sort.cpp
@@ -0,0 +1,20 @@
+void insertionSort(int arr[], int n);
+void quickSort(int arr[], int n);
+void mergeSort(int arr[], int n);
+void heapSort(int arr[], int n);
+
+void compositeSort(int arr[], int n)
+{
+    if (n <= 32)
+    {
+        insertionSort(arr, n);
+    }
+    else if (n <= 1500)
+    {
+        quickSort(arr, n);
+    }
+    else
+    {
+        mergeSort(arr, n);
+    }
+}
